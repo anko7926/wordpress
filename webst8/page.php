974e167db14b836bff788d64b1d1e26bf7c480f3
anko7926 <97778283+anko7926@users.noreply.github.com>
2024-11-19 20:23:15 +0900

    Add files via upload

---
+++ webst8/page.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <title>オリジナルテーマ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="mystyle.css" />
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" ></script>
+  </head>
+  <body>
+  <?php get_header(); ?>
+<!-- 上記が追記するコード -->
+
+<section id="content">
+...
+    <header class="bg-secondary-subtle">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">オリジナルテーマ</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.html">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">ABOUT</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">BLOG</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">CONTACT</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <div id="top-vg" class="container-fluid">
+      <img src="image\top.jpg" alt="トップページ画像" />
+    </div>
+    <section id="content">
+      <div id="content-wrap" class="container row pt-3">
+        <div id="main" class="col-md-9">
+        <?php get_header(); ?>
+<section id="content">
+  <div id="content-wrap" class="container row pt-3">
+    <div id="main" class="col-md-9">
+      <?php // ループ開始
+      if (have_posts()) :
+        while (have_posts()) : the_post();
+      ?>
+          <h1><?php the_title(); ?></h1> <!-- タイトル取得 -->
+          <section>
+            <?php the_content(); ?> <!-- 本文取得 -->
+          </section>
+      <?php // ループ終了
+        endwhile;
+      endif;
+      ?>
+    </div>
+    <div id="sidebar" class="col-md-3">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
+        </div>
+      </div>
+    <div id="sidebar" class="col-md-3">
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+</section>
+<?php get_footer(); ?>
+          </div>
+          <p class="text-center">お申し込みは<a href="#">こちらから</a> <br /><br /></p>
+        </div>
+        <div id="sidebar" class="col-md-3">
+          <section>
+            <h2>ホームページ作成</h2>
+            <p><img src="image\banar.jpg" alt="バナー画像" /></p>
+          </section>
+          <section>
+            <h2>TikTok配信中</h2>
+            <p><img src="image\tiktok.jpg" alt="Tiktokサンプル画像" /></p>
+          </section>
+        </div>
+      </div>
+    </section>
+    ...
+</section>
+ <!-- 下記が追記するコード -->
+<?php get_footer(); ?>
+    <footer>
+      <div class="container">
+        <p class="text-center mb-0">Copyright © PAM All Rights Reserved.</p>
+      </div>
+      ...
+ <div id="sidebar" class="col-md-3">
+   <?php get_sidebar(); ?>
+   <!-- 上記が追記するコード -->
+ </div>
+...
+    </footer>
+  </body>
+</html>
